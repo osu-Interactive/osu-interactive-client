f6b8011510d61a593cdcdf897c637cc7eb390685
Ruichimi <thekeykeyloy@gmail.com>
2026-04-10 20:46:41 +0200

    implement some auth logic via osu api code grant and own server

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,3 +1,34 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { fetchOsuApiAuthLink, loginWithOsuApiCode } from '../services/client.ts'
+import { ref, onMounted } from 'vue'
 
-<template>Login</template>
+const code = ref<string | null>(null)
+
+onMounted(async () => {
+    const url = new URL(window.location.href)
+    const params = url.searchParams
+
+    if (params.has('code')) {
+        code.value = params.get('code')
+
+        params.delete('code')
+
+        const newUrl = url.pathname + (params.toString() ? `?${params}` : '')
+        window.history.replaceState({}, '', newUrl)
+    }
+
+    if (code.value) {
+        const loginData = await loginWithOsuApiCode(code.value)
+
+        console.log(loginData)
+    }
+})
+
+async function redirectToOsuApiLogin(): Promise<void> {
+    window.location.href = await fetchOsuApiAuthLink()
+}
+</script>
+
+<template>
+    <button @click="redirectToOsuApiLogin">Login</button>
+</template>
