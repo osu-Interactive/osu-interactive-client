211fc21b80aac2228ed2abeaf5d9a5ce61f56d73
YehorCringe <nosn545@gmail.com>
2026-04-16 20:22:04 +0300

    remove unused methods from client

--- src/services/client.ts
+++ src/services/client.ts
@@ -32,51 +32,6 @@ export async function loginWithOsuApiCode(
     }
 }
 
-interface SaveSkillsRequest {
-    skills: number[]
-}
-
-interface SaveSkillsResponse {
-    status: string
-}
-
-interface SaveModsRequest {
-    mods: number[]
-}
-
-interface SaveModsResponse {
-    status: string
-}
-export async function saveUserSkills(
-    skills: number[],
-): Promise<SaveSkillsResponse> {
-    try {
-        const res = await axios.post<SaveSkillsResponse>(
-            `${config.server_url}/survey/save`,
-            { skills } as SaveSkillsRequest,
-        )
-
-        return res.data
-    } catch (err) {
-        console.error(err)
-        throw err
-    }
-}
-
-export async function saveUserMods(mods: number[]): Promise<SaveModsResponse> {
-    try {
-        const res = await axios.post<SaveSkillsResponse>(
-            `${config.server_url}/survey/save`,
-            { mods } as SaveModsRequest,
-        )
-
-        return res.data
-    } catch (err) {
-        console.error(err)
-        throw err
-    }
-}
-
 interface SurveyResultRequest {
     skillsets: number[]
     mods: number[]
